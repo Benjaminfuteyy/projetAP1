f07cf4c077492b56e755a4ed3a9330d657142f65
NathanForestier <32866462+NathanForestier@users.noreply.github.com>
2017-11-24 15:28:30 +0100

    Update projet.java

--- projet.java
+++ projet.java
@@ -2,5 +2,7 @@ class projet extends Program {
     
     //variables globales
     String[] niveau1 = new String[] {}
-    //
+    //git remote add origin https://github.com/NathanForestier/projetAP1.git
+    //git config --global http.proxy http://cache.univ-lille1.fr:3128
+
 }
